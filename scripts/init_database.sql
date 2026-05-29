6c22641fc3e2e16e3b854807a9c467c4370cf136
Mel Angelo Mendez <113498039+melmendez37@users.noreply.github.com>
2026-05-29 22:42:30 +0800

    Updated script to PostgreSQL

--- scripts/init_database.sql
+++ scripts/init_database.sql
@@ -2,40 +2,22 @@
 =============================================================
 Create Database and Schemas
 =============================================================
-This script creates a new database named 'DataWarehouse' after checking if it already exists. 
+This script creates a new database named 'datawarehouse' after checking if it already exists. 
 If the database exists, it is dropped and recreated. Additionally, the script sets up three schemas 
 within the database: 'bronze', 'silver', and 'gold'.
 	
 WARNING:
-    Running this script will drop the entire 'DataWarehouse' database if it exists. 
+    Running this script will drop the entire 'datawarehouse' database if it exists. 
     All data in the database will be permanently deleted. Proceed with caution 
     and ensure you have proper backups before running this script.
 */
 
-USE master;
-GO
+DROP DATABASE IF EXISTS "datawarehouse";
 
--- Drop and recreate the 'DataWarehouse' database
-IF EXISTS (SELECT 1 FROM sys.databases WHERE name = 'DataWarehouse')
-BEGIN
-    ALTER DATABASE DataWarehouse SET SINGLE_USER WITH ROLLBACK IMMEDIATE;
-    DROP DATABASE DataWarehouse;
-END;
-GO
+CREATE DATABASE "datawarehouse";
 
--- Create the 'DataWarehouse' database
-CREATE DATABASE DataWarehouse;
-GO
+\c datawarehouse;
 
-USE DataWarehouse;
-GO
-
--- Create Schemas
 CREATE SCHEMA bronze;
-GO
-
 CREATE SCHEMA silver;
-GO
-
 CREATE SCHEMA gold;
-GO
